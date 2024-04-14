8f7b5fc15f09d9df4221ec762b5daa66515dfc93
smac4343 <smac4343@gmail.com>
2024-04-14 14:55:17 +0300

    Add some changes

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,19 @@
+import os
+
 from django.http import HttpResponse
+
+
+class Test:
+    pass
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index_all():
+    value = "Test"
+    for item in values:
+        print("gf")
+
+        return 0
